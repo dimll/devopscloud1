e82163684c54e6c8bfbc6949e56af634fd20205e
Dimitris <dim-laskos@hotmail.com>
2023-02-17 00:58:54 +0100

    Added functionality for mongoDB

--- swagger_server/server/student_service.py
+++ swagger_server/server/student_service.py
@@ -4,11 +4,13 @@ from functools import reduce
 
 from tinydb import TinyDB, Query
 
+
 db_dir_path = tempfile.gettempdir()
 db_file_path = os.path.join(db_dir_path, "students.json")
 student_db = TinyDB(db_file_path)
 
-// Test
+# MongoDB setup
+
 
 def add(student=None):
     queries = []
