eb7d6a75bf4716f5b408b9a459967d03abd0f3b2
Dimitris <dim-laskos@hotmail.com>
2023-02-16 13:45:05 +0100

    Branch test

--- swagger_server/server/student_service.py
+++ swagger_server/server/student_service.py
@@ -8,6 +8,7 @@ db_dir_path = tempfile.gettempdir()
 db_file_path = os.path.join(db_dir_path, "students.json")
 student_db = TinyDB(db_file_path)
 
+// Test
 
 def add(student=None):
     queries = []
